d32df5bb21af638fd3e6959e786431973e3b1517
Lucas Vazquez <lucas5zvazquez@gmail.com>
2019-08-16 01:08:52 -0300

    Vox

--- src/java/cepita/respuesta.java
+++ src/java/cepita/respuesta.java
@@ -11,8 +11,17 @@ package cepita;
  */
 class respuesta {
 
-    static void setAttribute(String error, String hay_campos_vacios) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    static void setAttribute(String error, String motivo_del_error) {
+        // Vi esto en vox, no sé Java por lo que la sintaxis puede estar mal,
+        // pero acá te dejo la posible solución a tu problema.
+        // Podes quitar la variable "error" de esta función. Vi que la llamas en
+        // "SrvCalidad.java", y siempre que lo haces pasas la variable error con
+        // el valor "error" y es medio al pedo.
+        // Podés quitar esa variable de esta función y dejar el print de la
+        // siguiente manera:
+        // System.out.print("error: %s", motivo_del_error);
+        System.out.print("%s: %s", error, motivo_del_error);
+        // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
